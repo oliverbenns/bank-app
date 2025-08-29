ac732a1e096353fc034cbf22eba73c7782f3d6a8
Oliver Benns <oliver@benns.co>
2025-08-29 14:40:47 +0700

    Header remove on login

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,18 @@ export default () => {
   return (
     <>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(auth)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </>
